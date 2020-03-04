7fe17f8d0151f8a3da4f293eb4895d297701b346
punitkhiyara98 <pkhiyara@terpmail.umd.edu>
2020-03-04 12:04:22 -0500

    Updated & Formatted JS

--- lab_4/beginner-html-site-styled-gh-pages/scripts/main.js
+++ lab_4/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,30 +1,34 @@
-let myImage = document.querySelector('img');
+let myImage = document.querySelector("img");
 
 myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
-    } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
-    }
-}
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
 
 function setUserName() {
-    let myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
-  }
-
-  if(!localStorage.getItem('name')) {
+  let myName = prompt("Please enter your name.");
+  if (!myName || myName === null) {
     setUserName();
   } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+    localStorage.setItem("name", myName);
+    myHeading.innerHTML = "Mozilla is cool, " + myName;
   }
+}
 
-  myButton.onclick = function() {
-    setUserName();
-  }
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla is cool, " + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+};
