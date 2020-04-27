af6dd3798916b6a91cd6fc85086a041ff541fad5
punitkhiyara98 <pkhiyara@terpmail.umd.edu>
2020-04-26 20:09:34 -0500

    Updates

--- lab_10/server.js
+++ lab_10/server.js
@@ -1,17 +1,17 @@
 // These are our required libraries to make the server work.
 
-import express from "express";
-import fetch from "node-fetch";
+import express from 'express';
+import fetch from 'node-fetch';
 
 // const sqlite3 = require('sqlite3').verbose(); // We're including a server-side version of SQLite, the in-memory SQL server.
 // const open = require(sqlite).open; // We're including a server-side version of SQLite, the in-memory SQL server.
 
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import writeUser from "./libraries/writeuser";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import writeUser from './libraries/writeuser';
 
 const dbSettings = {
-  filename: "./tmp/database.db",
+  filename: './tmp/database.db',
   driver: sqlite3.Database,
 };
 
@@ -20,10 +20,20 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
+
+async function dbBoot() {
+  console.log('async DB boot');
+  const db = await open(dbSettings);
+
+  await db.exec('CREATE TABLE IF NOT EXISTS user (name)');
+  await db.exec('INSERT INTO user VALUES ("Beth")');
+  const result = await db.get('SELECT * FROM user');
+  console.log('Expected result', result);
+}
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = ""; // Enter the URL for the data you would like to retrieve here
+  const baseURL = ''; // Enter the URL for the data you would like to retrieve here
 
   // Your Fetch API call starts here
   // Note that at no point do you "return" anything from this function -
@@ -36,7 +46,7 @@ function processDataForFrontEnd(req, res) {
     })
     .catch((err) => {
       console.log(err);
-      res.redirect("/error");
+      res.redirect('/error');
     });
 }
 
@@ -44,33 +54,34 @@ function processDataForFrontEnd(req, res) {
 // or we'll end up with spelling errors in our endpoints.
 //
 app
-  .route("/api")
+  .route('/api')
   .get((req, res) => {
     // processDataForFrontEnd(req, res)
-    (async () => {
-      const db = await open(dbSettings);
-      const result = await db.all("SELECT * FROM user");
-      console.log("Expected result", result);
-      res.json(result);
-    })();
+    // (async () => {
+    //   const db = await open(dbSettings);
+    //   const result = await db.all('SELECT * FROM user');
+    //   console.log('Expected result', result);
+    //   res.json(result);
+    // })();
   })
   .post((req, res) => {
-    console.log("/api post request", req.body);
-    if (!req.body.name) {
-      console.log(req.body);
-      res.status("418").send("something went wrong, additionally i am a teapot");
-    } else {
-      writeUser(req.body.name, dbSettings)
-      .then((result) => {
-        console.log(result);
-        res.send("your request was successful"); // simple mode
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
+    console.log('/api post request', req.body);
+    // if (!req.body.name) {
+    //   console.log(req.body);
+    //   res.status('418').send('something went wrong, additionally i am a teapot');
+    // } else {
+    //   writeUser(req.body.name, dbSettings)
+    //     .then((result) => {
+    //       console.log(result);
+    //       res.send('your request was successful'); // simple mode
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    // }
   });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
+  dbBoot();
 });
